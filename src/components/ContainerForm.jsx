c6306a5a8af9a149d40058837735f9ac415c7d5a
mohd noman <mohdnoman2751@gmail.com>
2024-04-19 00:37:34 +0530

    .

--- src/components/ContainerForm.jsx
+++ src/components/ContainerForm.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import { Box, Text, CloseButton } from "@chakra-ui/react";
 import { useDrag } from 'react-dnd';
